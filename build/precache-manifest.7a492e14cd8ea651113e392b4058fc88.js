1a49ef230075c2c441345666de11dcc1a3554d44
Edward Ottorino <edward.ottorino@gmail.com>
2020-03-31 16:57:05 +1100

    'oops-forgot'

---
+++ build/precache-manifest.7a492e14cd8ea651113e392b4058fc88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397e2b0438bc3ba893b6857b6103c398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed5081e984d8707d12a",
+    "url": "/static/css/main.642f2dbd.chunk.css"
+  },
+  {
+    "revision": "50412834e476e38b23f2",
+    "url": "/static/js/2.aeb1cb1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aeb1cb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed5081e984d8707d12a",
+    "url": "/static/js/main.b56e4601.chunk.js"
+  },
+  {
+    "revision": "1adf760d908c3cec6448",
+    "url": "/static/js/runtime-main.2da45dab.js"
+  }
+]);
